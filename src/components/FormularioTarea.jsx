4907e5dfadd740723d5f7a1e3684c7eb87247a8b
Santiago <santiagof00@gmail.com>
2022-09-26 11:43:33 -0300

    Agrego maquetado

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { ListaTarea } from "./ListaTarea";
 
 export const FormularioTarea = () => {
   return (
-    <div>Aqui va un formulario</div>
-  )
-}
+    <>
+        <Form>
+            <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
+                <Form.Control type="text" placeholder="Ingrese una tarea" />
+                <Button variant="primary" type="submit">
+                    Enviar
+                </Button>
+            </Form.Group>
+        </Form>
+        <ListaTarea></ListaTarea>
+    </>
+  );
+};
--- src/components/ListaTarea.jsx
+++ src/components/ListaTarea.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
+import { ItemTarea } from './ItemTarea'
 
 export const ListaTarea = () => {
   return (
     <ListGroup>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+       <ItemTarea></ItemTarea>
     </ListGroup>
   )
 }
