a7ced6619cc066356f3116866b9cd22b4df5eebd
Santiago <santiagof00@gmail.com>
2022-09-26 11:59:57 -0300

    Agrego tareas al arreglo y agrego el value vacio cada vez que ingreso tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { ListaTarea } from "./ListaTarea";
 
 export const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [arregloTareas, setArregloTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArregloTareas([...arregloTareas, tarea]);
+    setTarea('');
+  };
+
   return (
     <>
-        <Form>
-            <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="Ingrese una tarea" />
-                <Button variant="primary" type="submit">
-                    Enviar
-                </Button>
-            </Form.Group>
-        </Form>
-        <ListaTarea></ListaTarea>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTarea></ListaTarea>
     </>
   );
 };
