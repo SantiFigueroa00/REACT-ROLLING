a42aebd3f43114e88dafc374795d2d27a1ce0c9f
Santiago <santiagof00@gmail.com>
2022-10-03 01:12:50 -0300

    Agrego el ciclo de vida para guardar localstorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { ListaTarea } from "./ListaTarea";
 
 export const FormularioTarea = () => {
+
+  const tareasLS = JSON.parse(localStorage.getItem('listaTareas')) || [];
   const [tarea, setTarea] = useState("");
-  const [arregloTareas, setArregloTareas] = useState([]);
+  const [arregloTareas, setArregloTareas] = useState(tareasLS);
+
+  useEffect(()=>{
+    console.log('Montaje')
+    localStorage.setItem('listaTareas', JSON.stringify(arregloTareas));
+  },[arregloTareas])
 
   const handleSubmit = (e) => {
     e.preventDefault();
